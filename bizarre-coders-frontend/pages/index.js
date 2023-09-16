83952c05c2897bfa9273c93718936aca91f0d953
ARYAN-CODES-STAR <aryansh0004@gmail.com>
2023-09-16 12:01:05 +0530

    Add Navbar page and routing added

--- bizarre-coders-frontend/pages/index.js
+++ bizarre-coders-frontend/pages/index.js
@@ -1,8 +1,13 @@
-import Image from "next/image";
+// import Image from "next/image";
 import { Inter } from "next/font/google";
+import { useRouter } from 'next/router';
+
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return <div></div>;
+  const router = useRouter();
+  return <div>
+     <button onClick={() => router.push('/Navbar')}>Navbar</button>
+  </div>;
 }
