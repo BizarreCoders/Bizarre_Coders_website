8c3218580be6a30b953f8d49c513060365d8b486
ARYAN-CODES-STAR <aryansh0004@gmail.com>
2023-10-29 14:46:00 +0530

    [Hero][Fix]Chatbot feature removed

--- bizarre-coders-frontend/pages/Hero.js
+++ bizarre-coders-frontend/pages/Hero.js
@@ -1,21 +1,18 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import Image from "next/image";
 import githublogo from "./assets/github.png";
 import img1 from "./assets/BC_1.jpeg";
-// import axios from "axios";
 // import Chatbot from "./Chatbot";
 
 function Hero() {
-  const [showChatbot, setShowChatbot] = useState(0);
+//   const [showChatbot, setShowChatbot] = useState(false);
 
-  const chatbotToggle = () => {
-    setShowChatbot(showChatbot+1);
-  }
-  // const chatbotToggle = () => {
-  //   setShowChatbot('Sharma');
-  // }
+//   const handleChatbotToggle = () => {
+//     setShowChatbot(!showChatbot);
+//   };
+
   return (
     <div className=" items-center min-h-screen py-16 bg-canva_nav ">
       <div className="max-w-2xl px-8">
@@ -44,13 +41,10 @@ function Hero() {
           </div>
         </Carousel>
       </div>
-      <button className="text-whitee" onClick={chatbotToggle}> 
-      Hey {showChatbot};
+      <button className="text-whitee">
+        Open Chat
       </button>
-      {/* <button onClick={handleClick}>
-      You pressed me {count} times
-    </button> */}
-      {/* {showChatbot && <Chatbot />} */}
+      {/* {showChatbot && <Chatbot /> } */}
     </div>
   );
 }
