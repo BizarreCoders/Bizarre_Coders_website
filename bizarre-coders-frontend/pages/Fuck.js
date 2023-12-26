9c2f2359a6297d52a0008cec6204c41b3196385a
ARYAN-CODES-STAR <aryansh0004@gmail.com>
2023-12-26 12:17:42 +0530

    [refactor][Hero][Team] Experiment with animation

--- bizarre-coders-frontend/pages/Fuck.js
+++ bizarre-coders-frontend/pages/Fuck.js
@@ -1,33 +1,49 @@
-import React from "react";
-import { Animate, AnimateKeyframes, AnimateGroup } from "react-simple-animate";
+// import React from "react";
+// import { Animate, AnimateKeyframes, AnimateGroup } from "react-simple-animate";
 
-export default () => (
-  <>
-    {/* animate individual element. */}
-    <Animate play start={{ opacity: 0 }} end={{ opacity: 1 }}>
-      <h1>React simple animate</h1>
-    </Animate>
+// export default () => (
+//   <>
+//     {/* animate individual element. */}
+//     <Animate play start={{ opacity: 0 }} end={{ opacity: 1 }}>
+//       <h1>React simple animate</h1>
+//     </Animate>
     
-    {/* animate keyframes with individual element. */}
-    <AnimateKeyframes
-      play
-      iterationCount="infinite"
-      keyframes={["opacity: 0", "opacity: 1"]}
-    >
-      <h1>React simple animate with keyframes</h1>
-    </AnimateKeyframes>
+//     {/* animate keyframes with individual element. */}
+//     <AnimateKeyframes
+//       play
+//       iterationCount="infinite"
+//       keyframes={["opacity: 0", "opacity: 1"]}
+//     >
+//       <h1>React simple animate with keyframes</h1>
+//     </AnimateKeyframes>
     
-    {/* animate group of animation in sequences */}
-    <AnimateGroup play>
-      <Animate start={{ opacity: 0 }} end={{ opacity: 1 }} sequenceIndex={0}>
-        first
-      </Animate>
-      <Animate start={{ opacity: 0 }} end={{ opacity: 1 }} sequenceIndex={1}>
-        second
-      </Animate>
-      <Animate start={{ opacity: 0 }} end={{ opacity: 1 }} sequenceIndex={2}>
-        third
-      </Animate>
-    </AnimateGroup>
-  </>
-);
+//     {/* animate group of animation in sequences */}
+//     <AnimateGroup play>
+//       <Animate start={{ opacity: 0 }} end={{ opacity: 1 }} sequenceIndex={0}>
+//         first
+//       </Animate>
+//       <Animate start={{ opacity: 0 }} end={{ opacity: 1 }} sequenceIndex={1}>
+//         second
+//       </Animate>
+//       <Animate start={{ opacity: 0 }} end={{ opacity: 1 }} sequenceIndex={2}>
+//         third
+//       </Animate>
+//     </AnimateGroup>
+//     {/* <MotionAnimate animation='scrollPosition' xPos={[1400, -600]}>
+//                         <div>Scroll Position</div>
+//                       </MotionAnimate> */}
+//   </>
+// );
+
+import React from 'react';
+import { MotionAnimate } from 'react-motion-animate';
+
+const Example = () => {
+  return (
+    <MotionAnimate>
+      <div>Component you want to animate</div>
+    </MotionAnimate>
+  );
+};
+
+export default Example;
