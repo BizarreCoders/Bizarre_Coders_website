6c038c67f2cd3ae12df4b04d2da14ed8e54ba612
ARYAN-CODES-STAR <aryansh0004@gmail.com>
2024-01-14 23:50:10 +0530

    [refactor][Team.js] Some changes done

--- bizarre-coders-frontend/pages/Team.js
+++ bizarre-coders-frontend/pages/Team.js
@@ -1,21 +1,4 @@
-/*
-  This example requires some changes to your config:
-  
-  ```
-  // tailwind.config.js
-  module.exports = {
-    // ...
-    plugins: [
-      // ...
-      require('@tailwindcss/aspect-ratio'),
-    ],
-  }
-  ```
-*/
-
-
-
-const teamMembers = [ 
+const teamMembers = [
   {
     name: "Aryan Sharma",
     role: "Web3 Developer",
@@ -82,7 +65,7 @@ export default function Example() {
               <i>Our Core Team Members</i>
             </h2>
             {/* <AnimateOnChange>Brilliantnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn</AnimateOnChange> */}
-            <p className="text-xl text-gray-500">  vv</p>
+            <p className="text-xl text-gray-500"> vv</p>
           </div>
           {/* <ul
             role="list"
@@ -148,9 +131,6 @@ export default function Example() {
             ))}
           </ul> */}
 
-
-
-          
           {teamMembers.map((member) => (
             <div
               key={member.id}
@@ -163,27 +143,26 @@ export default function Example() {
                 alt={member.name}
               />
               <div className="px-6 py-4">
-
                 {/* <div className="font-bold text-xl mb-2">{member.name}</div> */}
                 <p className="text-gray-700 text-base">{member.role}</p>
               </div>
-            <ul role="list" className="flex space-x-5"> 
-           <li>
-              <a
-                href={teamMembers.twitterUrl}
-                className="text-gray-400 hover:text-gray-500"
-              >
-                <span className="sr-only">Twitter</span>
-                <svg
-                  className="h-5 w-5"
-                  aria-hidden="true"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                >
-                  <path d="M6.29 18.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0020 3.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.073 4.073 0 01.8 7.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 010 16.407a11.616 11.616 0 006.29 1.84" />
-                </svg>
-              </a>
-              </li>
+              <ul role="list" className="flex space-x-5">
+                <li>
+                  <a
+                    href={teamMembers.twitterUrl}
+                    className="text-gray-400 hover:text-gray-500"
+                  >
+                    <span className="sr-only">Twitter</span>
+                    <svg
+                      className="h-5 w-5"
+                      aria-hidden="true"
+                      fill="currentColor"
+                      viewBox="0 0 20 20"
+                    >
+                      <path d="M6.29 18.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0020 3.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.073 4.073 0 01.8 7.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 010 16.407a11.616 11.616 0 006.29 1.84" />
+                    </svg>
+                  </a>
+                </li>
               </ul>
             </div>
           ))}
